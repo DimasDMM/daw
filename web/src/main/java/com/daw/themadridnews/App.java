f58681f0fa4e1cbc18fb45def3fce4ebc780957f
Dimas <dimas.95@hotmail.com>
2017-02-19 11:11:18 +0100

    Hola Mundo Spring Boot

---
+++ web/src/main/java/com/daw/themadridnews/App.java
@@ -0,0 +1,11 @@
+package com.daw.themadridnews;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ web/src/main/java/com/daw/themadridnews/HolaController.java
@@ -0,0 +1,14 @@
+package com.daw.themadridnews;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HolaController {
+	@RequestMapping("/hola")
+	public String Hola(Model model) {
+		model.addAttribute("name","Mundo");
+		return "hola_template";
+	}
+}
