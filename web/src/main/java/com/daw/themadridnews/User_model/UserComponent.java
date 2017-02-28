08dfb6346eb96d7110ae1dae1340092f36415c3f
Felix Manuel <felixmanuel88@gmail.com>
2017-02-28 10:55:49 +0100

    User component
    Create a user component to control the session

---
+++ web/src/main/java/com/daw/themadridnews/User_model/UserComponent.java
@@ -0,0 +1,19 @@
+package com.daw.themadridnews.User_model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserComponent {
+    //Attributes
+    private String name;
+    private String last_name;
+    private String email;
+    private String password;
+    
+    //Constructor
+    public UserComponent(){        
+    }
+    
+    
+    
+}
