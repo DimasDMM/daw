6a61f953d513586162173dbdefdbdc6dc8e161a3
Dimas <dimas.95@hotmail.com>
2017-03-10 23:48:30 +0100

    (No funciona) Devuelto a su sitio controlador de categorias

--- web/src/main/java/com/daw/themadridnews/categories/CategoryController.java
+++ web/src/main/java/com/daw/themadridnews/categories/CategoryController.java
@@ -15,6 +15,23 @@ import com.daw.themadridnews.article.*;
 @Controller
 public class CategoryController {
 	
+	@Autowired
+	ArticleCategoryRepository categoryRepository;
+	
+	@Autowired
+	ArticleRepository articleRepository;
+	
+	@RequestMapping("/category/{cat}")
+	public String categories(Model model, @PathVariable String cat, Pageable pag){
+		
+		model.addAttribute("category",cat);
+		
+		Page<ArticleCategory> articles = categoryRepository.findAll(pag);
+				
+		return cat;
+		
+	}
+	
 	
 	
 }
