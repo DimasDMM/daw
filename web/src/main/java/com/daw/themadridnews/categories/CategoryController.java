c362602b2650a02ab1b8ce91885ee7cb6b122295
Alpargata <almapgtt@gmail.com>
2017-03-11 21:31:42 +0100

    Category

--- web/src/main/java/com/daw/themadridnews/categories/CategoryController.java
+++ web/src/main/java/com/daw/themadridnews/categories/CategoryController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.daw.themadridnews.article.*;
 
@@ -19,15 +18,13 @@ public class CategoryController {
 	@Autowired
 	ArticleRepository articleRepository;
 	
-	@Autowired
-	CategoryService categoryService;
-	
 	@RequestMapping("/categoria/{cat}/{npage}")
 	public String categories(Model model, @PathVariable String cat, Pageable pag, @PathVariable int npage){
 		
 		Page<Article> articles = articleRepository.findByCategory(cat, new PageRequest(npage,10));
 		
-		model.addAttribute("category",CategoryService.getName(cat));
+		model.addAttribute("categ",CategoryService.getName(cat));
+		model.addAttribute("cat",cat);
 		
 		model.addAttribute("articulos",ArticleView.castList(articles.getContent()));
 		
