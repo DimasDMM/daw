61cedb60228cbb9ce8efea61e5d0864a90309c35
Felix Manuel <felixmanuel88@gmail.com>
2017-03-12 10:55:56 +0100

    Logout
    Added code to logout

--- web/src/main/java/com/daw/themadridnews/SecurityConfiguration.java
+++ web/src/main/java/com/daw/themadridnews/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -14,7 +15,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public UserRepositoryAuthenticationProvider authenticationProvider;
 
-	@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
     	// Public pages
@@ -50,17 +51,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/publicista/anuncio/lista/{page}").hasAnyRole( "ADVERTISING", "ADMIN");
         http.authorizeRequests().antMatchers("/publicista/anuncio/lista/publicado").hasAnyRole( "ADVERTISING", "ADMIN");
         http.authorizeRequests().antMatchers("/publicista/anuncio/lista/eliminado").hasAnyRole( "ADVERTISING", "ADMIN");
-
+                
         // Login form
-        http.formLogin().loginPage("/portada");
+        http.formLogin().loginPage("/portada").and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/portada");
         http.formLogin().usernameParameter("email");
         http.formLogin().passwordParameter("password");
         http.formLogin().defaultSuccessUrl("/portada");
         http.formLogin().failureUrl("/error");
 
         // Logout
-        http.logout().logoutUrl("/logout");
-        http.logout().logoutSuccessUrl("/portada");
+        //http.logout().logoutUrl("/logout");
+        //http.logout().logoutSuccessUrl("/portada");
+        
     }
 
     @Override
