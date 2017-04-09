7d0874e56bb68cc69005d49c9d879894c90bb54c
Dimas <dimas.95@hotmail.com>
2017-04-09 16:49:32 +0200

    (No funciona) Intento de carrousel en portada

--- angular/src/app/components/home/home.component.ts
+++ angular/src/app/components/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
+import {Article} from "../../entity/article.entity";
 
 @Component({
   selector: 'app',
   templateUrl: 'home.component.html',
   styleUrls: []
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public carrouselArticles:Article[] = [];
+
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.initCarrouselArticles();
+  }
+
+  private initCarrouselArticles() {
+
+  }
 }
